99ca09b5e00a57c5c53451caa1714ce5102e6ebc
Destiny Li <destinylinz@gmail.com>
2021-03-08 10:09:55 +0000

    made prefix features more flexible

--- dinkbot.py
+++ dinkbot.py
@@ -6,6 +6,7 @@ import os
 from keep_alive import keep_alive
 
 TOKEN = os.getenv("TOKEN")
+PREFIX = 'd!'
 
 # dink dictionary
 d = ["destiny", "destiny", "destiny", "destiny", "destiny", "destiny", "dabbler", "dad", "daddy", "daggerman", "dairy", "dam", "damage", "dame", "damn", "damnation", "damsel", "dance", "dancer", "dancing", "dandelion", "danger", "danseur", "darbuka", "daring", "dark", "darling", "darn", "dash", "dashboard", "data", "date", "datum", "daughter", "dauphin", "davenport", "dawn", "day", "daybed", "daybreak", "daydreaming", "daylight", "daytime", "dazzler", "deacon", "deactivation", "dead", "deadline", "deadlock", "deadweight", "deae", "deal", "dealer", "dean", "dear", "dearth", "death", "deathbed", "debacle", "debate", "debating", "debauchery", "debility", "debt", "debunking", "debut", "debutante", "decade", "decadence", "decay", "deceased", "decedent", "deceit", "deceleration", "decency", "decentralization", "deception", "decimal", "decision", "deck", "decking", "declaration", "declarative", "decline", "declivity", "decolletage", "decomposition", "decompression", "decor", "decoration", "decorator", "decorum", "decrease", "decree", "decrement", "dedication", "deductibility", "deduction", "deed", "deepening", "deer", "defacing", "default", "defeat", "defeatism", "defect", "defection", "defence", "defendant", "defender", "defense", "deference", "deferent", "deferment", "defiance", "deficiency", "deficit", "definition", "deformation", "deformity", "degeneration", "degradation", "degree", "dehydration", "deification", "deity", "dejection", "delay", "delectation", "delegate", "delegation", "deliberation", "delicacy", "delight", "delineation", "delinquency", "delinquent", "delirium", "deliverance", "delivery", "dell", "deltoid", "deluge", "delusion", "deluxer", "demage", "demagnification", "demand", "demander", "demanding", "demarcation", "demeanor", "demineralization", "demise", "democracy", "democratization", "demography", "demolition", "demon", "demonstration", "demoralization", "demurrer", "demythologization", "den", "denial", "denomination", "denouement", "densitometry", "density", "dent", "dentist", "dentistry", "denunciation", "denying", "department", "departure", "dependence", "dependency", "dependent", "depersonalization", "depiction", "depletion", "deployment", "deposit", "deposition", "depot", "depravity", "depreciation", "depression", "deprivation", "depth", "deputy", "derangement", "derby", "derelict", "dereliction", "derision", "derivation", "derivative", "derrick", "derriere", "descendant", "descent", "description", "desecration", "desegregation", "desert", "desertion", "design", "designate", "designation", "designer", "designing", "desirability", "desire", "desk", "desolation", "despair", "despairing", "desperation", "despondency", "despot", "despotism", "dessert", "destination", "destiny", "destroyer", "destruction", "desuetude", "detachment", "detail", "detection", "detective", "detector", "detente", "detention", "detergency", "detergent", "deterioration", "determinability", "determinant", "determination", "determinism", "deterrence", "deterrent", "detestation", "detonation", "detractor", "detriment", "devastation", "developer", "development", "deviance", "deviation", "device", "devil", "devisee", "devotion", "dew", "dexamethasone", "dexterity", "diagnometer", "diagnosing", "diagnostic", "diagram", "dial", "dialect", "dialectic", "dialogue", "diam", "diameter", "diamond", "diaphragm", "diarrhea", "diarrhoea", "diary", "diathermy", "dice", "dichondra", "dichotomy", "dictator", "dictatorship", "diction", "dictionary", "dictum", "die", "diem", "diesel", "diet", "diethylaminoethyl", "diethylstilbestrol", "diety", "difference", "differentiability", "differential", "differentiation", "difficulty", "diffidence", "diffraction", "diffrunce", "diffusion", "dig", "digest", "digit", "digitalization", "dignity", "diisocyanate", "dilatation", "dilation", "dilemma", "dilettante", "diligence", "dill", "dilution", "dime", "dimension", "dimensioning", "dimethylglyoxime", "diminution", "din", "dinghy", "dingo", "dining", "dinner", "dinnertime", "dinnerware", "dinosaur", "diocese", "diorah", "dioxalate", "dioxide", "dip", "diphosphopyridine", "diplomacy", "diplomat", "dipper", "direction", "directionality", "directive", "directivity", "director", "directorate", "directorship", "directory", "dirge", "dirt", "disability", "disadvantage", "disaffection", "disagreement", "disappearance", "disappointment", "disapprobation", "disapproval", "disarmament", "disarray", "disassembly", "disaster", "disbelief", "disbursement", "disc", "discernment", "discharge", "disciple", "discipleship", "discipline", "disclaimer", "disclosure", "discomfort", "discontent", "discontinuance", "discontinuity", "discord", "discount", "discouragement", "discourse", "discoverer", "discovery", "discredit", "discrepancy", "discretion", "discrimination", "discussant", "discussion", "disdain", "disease", "disenfranchisement", "disengagement", "disfavor", "disgrace", "disguise", "disgust", "dish", "disharmony", "dishonesty", "dishonor", "dishwater", "disillusionment", "disinclination", "disintegration", "disinterest", "disk", "dislike", "dislocation", "disloyalty", "dismay", "dismemberment", "dismissal", "disobedience", "disorder", "disorganization", "disparagement", "disparity", "dispatch", "dispensary", "dispensation", "dispenser", "dispersal", "dispersement", "dispersion", "displacement", "display", "displeasure", "disposal", "disposition", "dispossession", "dispute", "disquiet", "disquietude", "disregard", "disrepair", "disrepute", "disrespect", "disruption", "dissatisfaction", "dissection", "dissemination", "dissension", "dissent", "dissenter", "disservice", "dissimulation", "dissociation", "dissolution", "dissolve", "distance", "distaste", "distension", "distillation", "distiller", "distinction", "distortion", "distraction", "distribution", "distributor", "distributorship", "district", "distrust", "disturbance", "disturber", "disunion", "disunity", "ditch", "ditcher", "ditty", "diva", "divan", "dive", "diver", "divergence", "diversification", "diversion", "diversity", "divertimento", "divestiture", "dividend", "divider", "divination", "divine", "diving", "divinity", "division", "divorce", "divorcee", "dline", "dock", "dockside", "doctor", "doctorate", "doctrinaire", "doctrine", "document", "documentary", "documentation", "dodge", "dog", "doghouse", "dogleg", "dogma", "dogmatism", "dogtrot", "dogwood", "doing", "doll", "dollar", "dollarette", "dolphin", "domain", "dome", "domestic", "domesticity", "domicile", "dominance", "domination", "dominion", "don", "donation", "donkey", "donor", "doom", "doomsday", "door", "doorbell", "doorkeeper", "doorknob", "doorman", "doormen", "doorstep", "doorway", "dope", "dormitory", "dosage", "dose", "dot", "dotting", "double", "doubleheader", "doubloon", "doubt", "doubte", "dough", "dove", "dowel", "dower", "down", "downfall", "downpayment", "downpour", "downtrend", "downturn", "dowry", "dozen", "draft", "draftee", "drafting", "drag", "dragger", "dragnet", "dragon", "drain", "drainage", "draining", "dram", "drama", "dramatist", "dramatization", "draper", "drapery", "draught", "draw", "drawback", "drawbridge", "drawer", "drawing", "drawl", "dread", "dream", "dreamer", "dresser", "dressing", "drier", "drift", "drill", "drilling", "drink", "drinker", "drinking", "drip", "drive", "driver", "driveway", "driving", "drizzle", "dromozoa", "drone", "drop", "drought", "drouth", "drove", "drudgery", "drug", "drugstore", "drum", "drumlin", "drummer", "drunk", "drunkard", "dryer", "drying", "drywall", "dualism", "duck", "duct", "ductwork", "dud", "due", "duel", "duet", "duf", "duffel", "duffer", "dugout", "duke", "dumbbell", "dump", "dumping", "dun", "dune", "dung", "dungeon", "duplicate", "duplication", "durability", "duration", "dusk", "dust", "dustbin", "dusting", "duty", "dwarf", "dweller", "dwelling", "dyerear", "dying", "dynamic", "dynamite", "dynamo", "dynasty", "dysentery", "dysplasia", "dystopia", "dystrophy"]
@@ -51,74 +52,79 @@ async def on_message(message):
 
     ################# COMMAND BASED FEATURES START HERE #################
 
-    # help
-    if message.content.startswith('d!info') or message.content.startswith('d!help')or message.content.startswith('d!commands'):
-        msg = 'd!hello \nd!info \nd!random \nd!dink <user> (use exact discord name, not nickname)\nd!doink <user>\nd!special <user>\nd!big \nd!face\nd!face2\nd!nsfw\n\n made by Destiny \nhttps://destiny-02.github.io/index.html'
-        embed_msg = discord.Embed(title="List of commands", description=msg, color=0x852d49)
-        await message.channel.send(embed = embed_msg)
+    if message.content.startswith(PREFIX):
+        command = message.content[len(PREFIX):].split()[0]
 
-    # big
-    if message.content.startswith('d!big'):
-        msg = ':regional_indicator_d::regional_indicator_i::regional_indicator_n::regional_indicator_k:'
-        await message.channel.send(msg)
+        try: 
+          command_2 = message.content[len(PREFIX):].split()[1]
+        except: 
+          command_2 = ""
 
-    # face
-    if message.content == 'd!face':
-        msg = ':eyes:'
-        bot_msg = await message.channel.send(msg)
-        await bot_msg.add_reaction('\U0001f445')
+        # help
+        if command == 'info' or command == 'help' or command == 'commands':
+            msg = 'd!hello \nd!info \nd!random \nd!dink <user> (use exact discord name, not nickname)\nd!doink <user>\nd!special <user>\nd!big \nd!face\nd!face2\nd!nsfw\n\n made by Destiny \nhttps://destiny-02.github.io/index.html'
+            embed_msg = discord.Embed(title="List of commands", description=msg, color=0x852d49)
+            await message.channel.send(embed = embed_msg)
 
-    # face2
-    if message.content.startswith('d!face2'):
-        msg = ':eyes:'
-        bot_msg = await message.channel.send(msg)
-        await bot_msg.add_reaction('\U0001f444')
+        # big
+        if command == 'big':
+            msg = ':regional_indicator_d::regional_indicator_i::regional_indicator_n::regional_indicator_k:'
+            await message.channel.send(msg)
 
-    # nsfw >:3
-    if message.content.startswith('d!nsfw'):
-        msg = ':eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n'
-        await message.channel.send(msg)
+        # face
+        if command == 'face':
+            msg = ':eyes:'
+            bot_msg = await message.channel.send(msg)
+            await bot_msg.add_reaction('\U0001f445')
 
-    # hello
-    if message.content.startswith('d!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
+        # face2
+        if command == 'face2':
+            msg = ':eyes:'
+            bot_msg = await message.channel.send(msg)
+            await bot_msg.add_reaction('\U0001f444')
 
-    # dink @user
-    if message.content.startswith('d!dink'):
-        given_name = message.content[7:]
-        try:
-            user = discord.utils.get(client.get_all_members(), name=given_name).id
-        except AttributeError:
-            return 
-        msg = 'You dinked <@!{}>'.format(user)
-        await message.channel.send(msg)
+        # nsfw >:3
+        if command == 'nsfw':
+            msg = ':eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n:eyes:\n:tongue:\n'
+            await message.channel.send(msg)
 
-    # doink @user
-    if message.content.startswith('d!doink'):
-        given_name = message.content[8:]
-        try:
-            user = discord.utils.get(client.get_all_members(), name=given_name).id
-        except AttributeError:
-            return
-        msg = 'You doinked <@!{}>'.format(user)
-        await message.channel.send(msg)
+        # hello
+        if command == 'hello':
+            msg = 'Hello {0.author.mention}'.format(message)
+            await message.channel.send(msg)
 
-    # special dink @user
-    if message.content.startswith('d!special'):
-        given_name = message.content[10:]
-        try:
-            user = discord.utils.get(client.get_all_members(), name=given_name).id
-        except AttributeError:
-            return
-        msg = 'You :sparkles: special dinked :sparkles: <@!{}>'.format(user)
-        await message.channel.send(msg)
+        # dink @user
+        if command == 'dink':
+            try:
+                user = discord.utils.get(client.get_all_members(), name=command_2).id
+            except AttributeError:
+                return 
+            msg = 'You dinked <@!{}>'.format(user)
+            await message.channel.send(msg)
 
-    # random dink
-    if message.content.startswith('d!random'):
-        msg = find_dink()
-        embed_msg = discord.Embed(title="dink stands for...", description=msg, color=0x852d49)
-        await message.channel.send(embed = embed_msg)
+        # doink @user
+        if command == 'doink':
+            try:
+                user = discord.utils.get(client.get_all_members(), name=command_2).id
+            except AttributeError:
+                return
+            msg = 'You doinked <@!{}>'.format(user)
+            await message.channel.send(msg)
+
+        # special dink @user
+        if command == 'special':
+            try:
+                user = discord.utils.get(client.get_all_members(), name=command_2).id
+            except AttributeError:
+                return
+            msg = 'You :sparkles: special dinked :sparkles: <@!{}>'.format(user)
+            await message.channel.send(msg)
+
+        # random dink
+        if command == 'random':
+            msg = find_dink()
+            embed_msg = discord.Embed(title="dink stands for...", description=msg, color=0x852d49)
+            await message.channel.send(embed = embed_msg)
 
     
     #################### NON-COMMAND FEATURES START HERE #######################
